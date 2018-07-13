7d85cf98fc8332fa607a44e51483a20519f7122b
phy2533 <phy253399933@qq.com>
2018-07-13 20:57:43 +0800

    日常提交

--- kafka/src/main/java/cn/puhy/kafka/stream/Aggregate.java
+++ kafka/src/main/java/cn/puhy/kafka/stream/Aggregate.java
@@ -1,12 +1,13 @@
 package cn.puhy.kafka.stream;
 
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KeyValueMapper;
-import org.apache.kafka.streams.kstream.Produced;
-import org.apache.kafka.streams.kstream.Serialized;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.*;
+
+import java.util.Properties;
 
 /**
  * 聚合操作
@@ -16,6 +17,7 @@ import org.apache.kafka.streams.kstream.Serialized;
  */
 public class Aggregate {
     public static void main(String[] args) {
+
         StreamsBuilder builder = new StreamsBuilder();
         KStream<String, String> stream1 = builder.stream("stream1");
 
@@ -26,6 +28,32 @@ public class Aggregate {
                 return new KeyValue<>(key, Integer.parseInt(value));
             }
         });
-        stream2.groupByKey(Serialized.with(Serdes.String(), Serdes.Integer()));
+        KGroupedStream<String, Integer> groupedStream = stream2.groupByKey(Serialized.with(Serdes.String(), Serdes.Integer()));
+        groupedStream.windowedBy(TimeWindows.of(1000).advanceBy(1000));
+        KTable<String, Integer> maxStream = groupedStream.aggregate(new Initializer<Integer>() {
+            //初始值
+            @Override
+            public Integer apply() {
+                return Integer.MIN_VALUE;
+            }
+        }, new Aggregator<String, Integer, Integer>() {
+            @Override
+            public Integer apply(String key, Integer value, Integer aggregate) {
+                return value > aggregate ? value : aggregate;
+            }
+        });
+
+        Properties config = new Properties();
+        //指定流处理应用的ID，必须配置
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "stream1");
+        //kafka地址
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        //KEY的序列化和反序列化类
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        //VALUE的序列化和反序列化类
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+        streams.start();
     }
 }
