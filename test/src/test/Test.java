5fef481b150948a25c9589957e845fdb6910d63a
puhongyu <phy253399933@qq.com>
2018-07-28 19:08:43 +0800

    20180728

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -1,24 +1,5 @@
 package test;
 
-
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import test.suning.Head;
-import test.suning.RequestBean;
-import test.suning.ResponseBean;
-
-import java.io.IOException;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.ServiceLoader;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-
 public class Test {
 
     static int a;
