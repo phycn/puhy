8a9db465c5a7bce1a20b4ddcb1ecef9cb75be005
phy2533 <phy253399933@qq.com>
2018-07-28 19:06:08 +0800

    日常提交

--- springcloud/config-client/src/main/java/cn/puhy/configclient/TestController.java
+++ springcloud/config-client/src/main/java/cn/puhy/configclient/TestController.java
@@ -1,7 +1,9 @@
 package cn.puhy.configclient;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Autowired
+    private Environment environment;
+
     @Value("${version}")
-    private String version;
+    String version;
 
     @GetMapping("/version")
     public String version() {
