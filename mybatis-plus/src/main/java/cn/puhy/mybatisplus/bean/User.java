5a818235d2f5e0a12744268da293a980791fa1a1
phy2533 <phy253399933@qq.com>
2018-08-20 21:50:12 +0800

    日常提交

---
+++ mybatis-plus/src/main/java/cn/puhy/mybatisplus/bean/User.java
@@ -0,0 +1,65 @@
+package cn.puhy.mybatisplus.bean;
+
+/**
+ * @author puhongyu
+ * 2018/5/15 06:42
+ */
+public class User {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String address;
+    private String phone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
