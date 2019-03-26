21e0c006babd9aa8d2f67c2f8b8c6ffebbf43495
puhongyu <phy2533@163.com>
2019-03-26 20:01:55 +0800

    20190326

--- call-chain/src/main/java/cn/puhy/callchain/MyTraceFilter.java
+++ call-chain/src/main/java/cn/puhy/callchain/MyTraceFilter.java
@@ -7,16 +7,13 @@ import com.alibaba.dubbo.rpc.*;
  * @author puhongyu
  * 2019-03-25 23:05
  */
-@Activate(
-        group = {"consumer"},
-        value = {"myTrace"}
-)
+@Activate
 public class MyTraceFilter implements Filter {
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         String traceId = TraceContext.getTraceId();
         RpcContext.getContext().setAttachment("traceId", traceId);
-        System.out.println("filter里的traceId: " + traceId);
+        System.out.println("发出的traceId: " + traceId);
         return invoker.invoke(invocation);
     }
 }
