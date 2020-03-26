923a0b3b278a234fa19ac1f0391f327f0594d46f
puhongyu <phy253399933@qq.com>
2020-03-26 21:50:37 +0800

    20200326

--- rabbitmq-springboot/src/main/java/cn/puhy/study/rabbitmqspringboot/consumer/Receiver.java
+++ rabbitmq-springboot/src/main/java/cn/puhy/study/rabbitmqspringboot/consumer/Receiver.java
@@ -1,8 +1,9 @@
 package cn.puhy.study.rabbitmqspringboot.consumer;
 
 import com.rabbitmq.client.Channel;
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -11,12 +12,13 @@ import java.io.IOException;
  * @author PUHY
  * 2019-07-09 0:30
  */
-@Component
+//@Component
 public class Receiver {
 
-    @RabbitListener(queues = "queue_demo")
+    @RabbitListener(queues = "queue_demo")  // 组合注解，可以配置@QueueBinding @Queue @Exchange
     public void receive(Message msg, Channel channel) throws IOException {
-        System.out.println("收到的消息：" + new String(msg.getBody()));
-        channel.basicAck(msg.getMessageProperties().getDeliveryTag(), true);
+        System.out.println("收到的消息：" + msg.getPayload());
+        long delieryTag = (long) msg.getHeaders().get(AmqpHeaders.DELIVERY_TAG);
+        channel.basicAck(delieryTag, false);
     }
 }
--- rabbitmq-springboot/src/main/java/cn/puhy/study/rabbitmqspringboot/producer/MQSender.java
+++ rabbitmq-springboot/src/main/java/cn/puhy/study/rabbitmqspringboot/producer/MQSender.java
@@ -1,5 +1,8 @@
 package cn.puhy.study.rabbitmqspringboot.producer;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,7 +17,69 @@ public class MQSender {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    //回调函数: confirm确认
+    final RabbitTemplate.ConfirmCallback confirmCallback = (correlationData, ack, cause) -> {
+        System.err.println("correlationData: " + correlationData);
+        System.err.println("ack: " + ack);
+        if (!ack) {
+            System.err.println("异常处理....");
+        }
+    };
+
+    //回调函数: return返回
+    final RabbitTemplate.ReturnCallback returnCallback =
+            (message, replyCode, replyText, exchange, routingKey) -> System.err.println("return exchange: " + exchange + ", routingKey: "
+                    + routingKey + ", replyCode: " + replyCode + ", replyText: " + replyText);
+
     public void send() {
-        rabbitTemplate.convertAndSend("exchange_demo", "routingkey_demo", "1212");
+        rabbitTemplate.setConfirmCallback(confirmCallback);
+        rabbitTemplate.setReturnCallback(returnCallback);
+        // 消息ID
+        CorrelationData correlationData = new CorrelationData("1234567890");
+
+        Bean bean = new Bean();
+        bean.setA("1");
+        bean.setB("2");
+        bean.setC("3");
+        rabbitTemplate.convertAndSend("exchange_demo", "routingkey_demo", JSON.toJSONString(bean), correlationData);
+    }
+}
+
+class Bean {
+    private String a;
+    private String b;
+    private String c;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
+    }
+
+    @Override
+    public String toString() {
+        return "Bean{" +
+                "a='" + a + '\'' +
+                ", b='" + b + '\'' +
+                ", c='" + c + '\'' +
+                '}';
     }
 }
